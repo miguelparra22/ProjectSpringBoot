cd04db28d4ff2df5fded716be6bff9bd974509e8
Miguel Angel <orjuelaparra@gmail.com>
2022-07-14 10:53:05 -0500

    Conecction with database mysql.

---
+++ src/main/java/com/example/AplicacionLogin/AplicacionLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.AplicacionLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AplicacionLogin/AplicacionLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AplicacionLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
